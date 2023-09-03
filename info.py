1da99b9899d64816f39388c2b8736c0c4b1190e0
God2020k <107332649+God2020k@users.noreply.github.com>
2023-09-03 15:46:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,9 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://stream-dfho.onrender.com".format(FQDN)
+    URL = "https://stream-dfho.onrender.com/".format(FQDN)
 else:
-    URL = "https://stream-dfho.onrender.com".format(FQDN)
+    URL = "https://stream-dfho.onrender.com/".format(FQDN)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
