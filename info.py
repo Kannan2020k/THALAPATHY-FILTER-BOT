ed61d263dbae89ee20c20ada4409fa30c75da3f2
God2020k <107332649+God2020k@users.noreply.github.com>
2023-09-03 12:28:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,10 +112,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://thalapathy-s8ws.onrender.com/".format(FQDN)
+    URL = "https://{}/".format(FQDN)
 else:
-    URL = "https://thalapathy-s8ws.onrender.com/".format(FQDN)
-
+    URL = "http://{}/".format(FQDN)
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
